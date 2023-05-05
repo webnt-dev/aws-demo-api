26c69a0e3c60708d30732e260753269bc5116e50
Bronislav Klučka <Bronislav.Klucka@webnt.dev>
2023-05-05 20:41:00 +0200

    feat: HTML subscription test

---
+++ tests/html/config.template.js
@@ -0,0 +1,7 @@
+const Config = {
+	API_KEY: 'XX-XXXXXXXXXXXXXXXXXXXXXXX', // API key
+	API_HOST: 'XXXXXXXXXXXXXXX.appsync-api.XXXXXXXXXXXX.amazonaws.com', // GraphQL endpoint HOST (NOT realtime endpoint)
+	API_URL: 'wss://XXXXXXXXXXXXXXX.appsync-realtime-api.XXXXXXXXXXXX.amazonaws.com/graphql' // GraphQL real-time endpoint HOST
+};
+
+export default Config;
